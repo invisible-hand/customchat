558e290048b80d288fd4101c0b2215af46edf159
Andrey Zagoruiko <2500mbytes@gmail.com>
2024-04-14 20:34:35 -0700

    history works, summary works + model to envs

--- app/api/route.js
+++ app/api/route.js
@@ -26,7 +26,8 @@ export async function POST(request) {
     messages.push({ role: 'user', content: input });
 
     const message = await anthropic.messages.create({
-      model: 'claude-3-haiku-20240307',
+      model: process.env.NEXT_PUBLIC_MODEL_NAME,
+      // model: 'claude-3-opus-20240229',
       max_tokens: 1024,
       messages,
     });
--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ const Home = () => {
   const [input, setInput] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
   const [savedChats, setSavedChats] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const storedChats = localStorage.getItem('chatHistory');
@@ -14,14 +15,11 @@ const Home = () => {
     }
   }, []);
 
-
-
   const handleNewChat = () => {
     setChatHistory([]);
     const newChat = [];
     setSavedChats([...savedChats, newChat]);
   };
-
 
   const handleKeyDown = (e) => {
     if (e.key === 'Enter' && (e.metaKey || e.ctrlKey)) {
@@ -29,49 +27,56 @@ const Home = () => {
     }
   };
 
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
-    if (chatHistory.length === 1 && chatHistory[0].user === '') {
-      // This is the first message of a new chat
-      const response = await fetch('/api', {
+    setInput('');
+    setIsLoading(true);
+
+    if (chatHistory.length === 0) {
+      const summaryResponse = await fetch('/api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ input: `Summarize in five words or less: ${input}` }),
       });
-  
-      if (response.ok) {
-        const data = await response.json();
-        const summary = data.response;
-  
-        // Add the summary as the name of the saved chat
-        const updatedSavedChats = [...savedChats.slice(0, -1), [{ user: input, bot: '' }]];
+
+      if (summaryResponse.ok) {
+        const summaryData = await summaryResponse.json();
+        const summary = summaryData.response;
+
+        const updatedSavedChats = [...savedChats, [{ user: summary, bot: '' }, { user: input, bot: '' }]];
         setSavedChats(updatedSavedChats);
         localStorage.setItem('chatHistory', JSON.stringify(updatedSavedChats));
-  
-        // Proceed with the regular handleSubmit logic
-        const botResponse = await handleRegularSubmit(input, updatedSavedChats[updatedSavedChats.length - 1]);
-        setChatHistory([{ user: input, bot: botResponse }]);
+
+        setChatHistory([{ user: input, bot: '' }]);
+
+        const botResponse = await handleRegularSubmit(input, [{ user: input, bot: '' }]);
+        const updatedChatHistory = [{ user: input, bot: botResponse }];
+        setChatHistory(updatedChatHistory);
+
+        const updatedSavedChatsWithResponse = savedChats.map((chat, index) =>
+          index === savedChats.length - 1 ? [{ user: summary, bot: '' }, ...updatedChatHistory] : chat
+        );
+        localStorage.setItem('chatHistory', JSON.stringify(updatedSavedChatsWithResponse));
+        setSavedChats(updatedSavedChatsWithResponse);
       } else {
-        console.error('Error:', response.statusText);
+        console.error('Error:', summaryResponse.statusText);
       }
     } else {
-      // Regular message in an existing chat
-      const botResponse = await handleRegularSubmit(input, chatHistory);
+      const botResponse = await handleRegularSubmit(input, [...chatHistory, { user: input, bot: '' }]);
       const updatedChatHistory = [...chatHistory, { user: input, bot: botResponse }];
       setChatHistory(updatedChatHistory);
-  
-      // Update the last entry in savedChats
-      const updatedSavedChats = [...savedChats.slice(0, -1), updatedChatHistory];
+
+      const updatedSavedChats = savedChats.map((chat, index) =>
+        index === savedChats.length - 1 ? updatedChatHistory : chat
+      );
       localStorage.setItem('chatHistory', JSON.stringify(updatedSavedChats));
       setSavedChats(updatedSavedChats);
     }
+
+    setIsLoading(false);
   };
-
-
 
   const handleRegularSubmit = async (input, chatHistory) => {
     const response = await fetch('/api', {
@@ -81,7 +86,7 @@ const Home = () => {
       },
       body: JSON.stringify({ input, chatHistory }),
     });
-  
+
     if (response.ok) {
       const data = await response.json();
       console.log('LLM Response:', data.response);
@@ -91,9 +96,6 @@ const Home = () => {
       return '';
     }
   };
-
-  
-
 
   const handleChatClick = (index) => {
     if (savedChats.length > 0) {
@@ -110,70 +112,72 @@ const Home = () => {
     setChatHistory([]);
   };
 
-
   return (
     <div className="container">
-      <div className="side-panel">
-        <h2>Chat History</h2>
-        <ul>
-          {savedChats.map((chat, index) => (
-            <li key={index} onClick={() => handleChatClick(index)}>
-              {chat[0]?.user || `Chat ${index + 1}`}
-            </li>
-          ))}
-        </ul>
-        <button className="clear-history-button" onClick={handleClearHistory}>
-          Clear History
-        </button>
-      </div>
-      <div className="main-content">
+      <header>
         <h1>Custom Claude</h1>
-        <button className="new-chat-button" onClick={handleNewChat}>
-          New Chat
-        </button>
-
-        <div className="chat-window">
-          {chatHistory.map((chat, index) => (
-            <div key={index}>
-              {index === 0 && <h2>{chat.user || 'Untitled Chat'}</h2>}
-              <p className="user-message">{chat.user}</p>
-              <div className="bot-message">
-                {chat.bot.split('```').map((part, i) => {
-                  if (i % 2 === 1) {
-                    return (
-                      <pre key={i} className="code-block">
-                        {part}
-                      </pre>
-                    );
-                  } else {
-                    return part.split('\n').map((line, j) => {
-                      if (line.startsWith('**') && line.endsWith('**')) {
-                        return <h2 key={`${i}-${j}`}>{line.slice(2, -2)}</h2>;
-                      } else if (line.startsWith('*') && line.endsWith('*')) {
-                        return <em key={`${i}-${j}`}>{line.slice(1, -1)}</em>;
-                      } else if (line.trim() === '') {
-                        return <br key={`${i}-${j}`} />;
-                      } else {
-                        return <p key={`${i}-${j}`}>{line}</p>;
-                      }
-                    });
-                  }
-                })}
-              </div>
-            </div>
-          ))}
+        <h2 className="model-name">Model: {process.env.NEXT_PUBLIC_MODEL_NAME}</h2>
+      </header>
+      <div className="content">
+        <div className="side-panel">
+          <h2>Chat History</h2>
+          <ul>
+            {savedChats.map((chat, index) => (
+              <li key={index} onClick={() => handleChatClick(index)} className="chat-item">
+                {chat[0]?.user ? chat[0].user.charAt(0).toUpperCase() + chat[0].user.slice(1) : `Chat ${index + 1}`}
+              </li>
+            ))}
+          </ul>
+          <button className="clear-history-button" onClick={handleClearHistory}>
+            Clear History
+          </button>
         </div>
-
-        <form onSubmit={handleSubmit}>
-          <textarea
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={handleKeyDown}
-            placeholder="Type your message..."
-            rows={4}
-          ></textarea>
-          <button type="submit">(Cmd + Enter) or Click</button>
-        </form>
+        <div className="main-content">
+          <button className="new-chat-button" onClick={handleNewChat}>
+            New Chat
+          </button>
+          <div className="chat-window">
+            {chatHistory.map((chat, index) => (
+              <div key={index}>
+                <p className="user-message">{chat.user}</p>
+                <div className="bot-message">
+                  {chat.bot.split('```').map((part, i) => {
+                    if (i % 2 === 1) {
+                      return (
+                        <pre key={i} className="code-block">
+                          {part}
+                        </pre>
+                      );
+                    } else {
+                      return part.split('\n').map((line, j) => {
+                        if (line.startsWith('**') && line.endsWith('**')) {
+                          return <h2 key={`${i}-${j}`}>{line.slice(2, -2)}</h2>;
+                        } else if (line.startsWith('*') && line.endsWith('*')) {
+                          return <em key={`${i}-${j}`}>{line.slice(1, -1)}</em>;
+                        } else if (line.trim() === '') {
+                          return <br key={`${i}-${j}`} />;
+                        } else {
+                          return <p key={`${i}-${j}`}>{line}</p>;
+                        }
+                      });
+                    }
+                  })}
+                </div>
+              </div>
+            ))}
+            {isLoading && <div className="loading-indicator">Loading...</div>}
+          </div>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={handleKeyDown}
+              placeholder="Type your message..."
+              rows={4}
+            ></textarea>
+            <button type="submit">(Cmd + Enter) or Click</button>
+          </form>
+        </div>
       </div>
     </div>
   );
